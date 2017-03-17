09c69d08013f37ad2e41d28e51b72738f806294a
Julian Trinh <julest@bu.edu>
2017-03-17 15:08:22 -0400

    Trying to add URLS

--- MealBoxV1/Search/views.py
+++ MealBoxV1/Search/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from .forms import SearchForm
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = SearchForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = SearchForm()
+
+    return render(request, 'search.html', {'form': form})

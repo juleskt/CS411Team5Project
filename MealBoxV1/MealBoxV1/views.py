300f4cd939cb34cded63aa60be4366b54a52dcbe
Julian Trinh <julest@bu.edu>
2017-03-17 22:40:08 -0400

    Got POST to submit to another page

--- MealBoxV1/MealBoxV1/views.py
+++ MealBoxV1/MealBoxV1/views.py
@@ -18,13 +18,32 @@ def search(request):
     # check whether it's valid:
     if form.is_valid():
       # process the data in form.cleaned_data as required
-      # ...
-      # redirect to a new URL:
       print (form)
-      return HttpResponseRedirect('/thanks/')
+      # redirect to a new URL:
+      return HttpResponseRedirect('/search-result/')
 
   # if a GET (or any other method) we'll create a blank form
   else:
     form = SearchForm()
 
   return render(request, 'search.html', {'form': form})
+
+
+def searchResult(request):
+  # if this is a POST request we need to process the form data
+  if request.method == 'POST':
+    # create a form instance and populate it with data from the request:
+    form = SearchForm(request.POST)
+    # check whether it's valid:
+    if form.is_valid():
+      # process the data in form.cleaned_data as required
+      print(form)
+      # redirect to a new URL:
+      # return HttpResponseRedirect('/search-result/')
+      return render(request, 'contact.html', {'form': form})
+
+  # if a GET (or any other method) we'll create a blank form
+  else:
+    form = SearchForm()
+
+  return render(request, 'contact.html', {'form': form})

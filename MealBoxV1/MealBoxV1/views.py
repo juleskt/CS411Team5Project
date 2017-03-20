35ab2aa7db28d36b4c66a309c44da70a1f136b1b
Julian Trinh <julest@bu.edu>
2017-03-20 12:07:49 -0400

    Cleaning up the code and adding comments for comprehension

--- MealBoxV1/MealBoxV1/views.py
+++ MealBoxV1/MealBoxV1/views.py
@@ -7,32 +7,27 @@ import json
 
 # add to your views
 def contact(request):
-  form_class = ContactForm
+    form_class = ContactForm
 
-  return render(request, 'search.html', {'form': form_class,})
+    return render(request, 'search.html', {'form': form_class,})
 
-
+# Belongs to the search page
 def search(request):
-  # if this is a POST request we need to process the form data
-  if request.method == 'POST':
-    # create a form instance and populate it with data from the request:
-    form = SearchForm(request.POST)
-    # check whether it's valid:
-    if form.is_valid():
-      # process the data in form.cleaned_data as required
-      print (form)
-      # redirect to a new URL:
-      r = requests.get('http://food2fork.com/api/search?key=86772c825c96cede81e423059eeac87f&q=' + str(request.POST.get('search', None)))
-      #decodedData = json.load(r.json())
-      #print (r.text)
-      #return HttpResponse(html)
-      return render(request, 'searchResults.html', {'objects': r.json()['recipes']})
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # Grab the search string by keyword from POST as defined in forms.py
+        searchString = str(request.POST.get('search', None))
+        # redirect to a new URL:
+        r = requests.get('http://food2fork.com/api/search?key=86772c825c96cede81e423059eeac87f&q=' + searchString)
+        # Serialize data for the searchResults.html template
+        print(r.json())
+        return render(request, 'searchResults.html', {'objects': r.json()['recipes'], 'searchString': searchString})
 
-  # if a GET (or any other method) we'll create a blank form
-  else:
-    form = SearchForm()
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = SearchForm()
 
-  return render(request, 'search.html', {'form': form})
+    return render(request, 'search.html', {'form': form})
 
 #class RecipesPage(generic.TemplateView):
 #    def get(self,request):

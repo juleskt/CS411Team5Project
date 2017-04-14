c294713f9129a641b3a5aea5c786790448f78227
alexcmcdaniel <acmc@bu.edu>
2017-04-14 15:28:00 -0400

    Oops

--- MealBoxV1/MealBoxV1/urls.py
+++ MealBoxV1/MealBoxV1/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from Search import views as SearchViews
 from Login import views as LoginViews
 from Recipes import views as RecipesViews
+from ShoppingList import views as ListViews
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -29,5 +30,7 @@ urlpatterns = [
     url(r'^search/$', SearchViews.search, name='search'),
     url(r'^search-result/$', SearchViews.search, name='search-result'),
     url(r'^add-recipe', SearchViews.addRecipe, name='add-recipe'),
-    url(r'^my-recipes/$', RecipesViews.showrecipes, name='my-recipes')
+    url(r'^shopping-list-from-search/$', SearchViews.addToShoppingList, name='add-to-shopping-list'),
+    url(r'^my-recipes/$', RecipesViews.showrecipes, name='my-recipes'),
+    url(r'^my-shopping-list/$', ListViews.index, name='my-list')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

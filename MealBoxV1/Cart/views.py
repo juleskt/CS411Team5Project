57d4a2d88032c6ccbd5009662296f43fb96359fa
alexcmcdaniel <acmc@bu.edu>
2017-04-20 00:55:09 -0400

    Very simple cart implementation lol

--- MealBoxV1/Cart/views.py
+++ MealBoxV1/Cart/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from amazon.api import AmazonAPI
+from SecretConfigs import *
+
+amazon = AmazonAPI(SecretConfigs.awsAccessKey(), SecretConfigs.awsSecretKey(), SecretConfigs.awsAssociateTag())
 
 def index(request):
-    recipeList = []
-    if 'shopping_list' not in request.session:
-        request.session['shopping_list'] = []
-    for recipe in request.session['shopping_list']:
-        recipeList.append(searchDBForRecipe(recipe))
-    return render(request, 'cart.html', {'recipes': recipeList})
+#    ingredientID = request.session['product_asin']
+    product = amazon.lookup(ItemId="0316067938")
+    item = {'offer_id': product.offer_id, 'quantity': 1}
+    if 'cartID' not in request.session or 'carthmac' not in request.session:
+        cart = amazon.cart_create(item)
+        request.session['cartID'] = cart.cart_id
+        request.session['carthmac'] = cart.hmac
+        print(str(request.session['cartID']) + "cart" + str(request.session['carthmac']))
+    else:
+        print(str(request.session['carthmac']))
+        print(request.session['cartID'])
+    purchaseURL = 'https://www.amazon.com/gp/cart/aws-merge.html?cart-id=' + str(request.session['cartID']) + '%26associate-id=' + str(SecretConfigs.awsAssociateTag()) + '%26hmac=' + str(request.session['carthmac']) + '%26AWSAccessKeyId=' + str(SecretConfigs.awsAccessKey())
+    return render(request, 'cart.html', {'purchase_url': purchaseURL})
